2c379953aee613c67d8df22f4d0ab87a91d8369d
oda-19 <dashenka-ostapenko@mail.ru>
2024-03-25 14:16:49 +0300

    докеры

--- licensing-service/src/test/java/com/example/license/DemoApplicationTests.java
+++ licensing-service/src/test/java/com/example/license/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.license;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootTest
 class DemoApplicationTests {
