c47e15097f8c34cef3fd76b2dc47f2f4b437f164
oda-19 <dashenka-ostapenko@mail.ru>
2024-06-10 04:33:45 +0300

    просто коммит

--- init.sql
+++ init.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS public.types
 (
     id serial NOT NULL,
-    name character varying(100),
+    name character varying(100) NOT NULL,
     CONSTRAINT types_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.licenses
 (
     id serial NOT NULL,
-    namePo character varying(255),
-    idType integer,
-    startDate Date,
-    endDate Date,
-    count integer,
+    namePo character varying(255) NOT NULL,
+    idType integer NOT NULL,
+    startDate Date NOT NULL,
+    endDate Date NOT NULL,
+    count integer NOT NULL,
     CONSTRAINT licenses_pkey PRIMARY KEY (id),
     CONSTRAINT idType FOREIGN KEY (idType)
         REFERENCES public.types (id) MATCH SIMPLE
